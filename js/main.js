3de1baba760541b6b64af52895255c199aa63ad6
TizianoN <hello@tizianonicolai.com>
2024-10-25 09:37:45 +0200

    feat: bug

--- js/main.js
+++ js/main.js
@@ -2,4 +2,4 @@ const outputNumber1El = document.getElementById('output-number-1');
 const outputNumber2El = document.getElementById('output-number-2');
 const outputNumber3El = document.getElementById('output-number-3');
 const outputNumber4El = document.getElementById('output-number-4');
-const outputNumber5El = document.getElementById('output-number-5');
+const outputNumber5El = document.getElementById('output-number-6');
