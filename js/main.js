4ff4326a6a4781fa268e8c0930343999e3f3327a
TizianoN <hello@tizianonicolai.com>
2024-10-25 09:38:48 +0200

    add true

--- js/main.js
+++ js/main.js
@@ -3,3 +3,5 @@ const outputNumber2El = document.getElementById('output-number-2');
 const outputNumber3El = document.getElementById('output-number-3');
 const outputNumber4El = document.getElementById('output-number-4');
 const outputNumber5El = document.getElementById('output-number-6');
+
+console.log(true);
